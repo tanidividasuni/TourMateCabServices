d8f00e92f5de277de4ca117c0b8c35a43d1a3a2d
tanidividasuni <tanidifonseka@gmail.com>
2025-10-18 10:24:29 +0530

    Initial commit

---
+++ start.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct vehicle{
+    string numberPlate;
+    string model;
+    string brand;
+    string type;
+    float charge;
+};
+
+struct sales{
+     string numberPlate;
+     string customerName;
+     int days;
+     float totalAmount;
+     string date;
+
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "C++17 Project Setup Successful." << endl;
+    return 0;
+
+}
